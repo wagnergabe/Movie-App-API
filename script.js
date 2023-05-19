cd6b8554d8d2f3d2fdcbf4f6a35600c8d2e30825
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-05-18 21:01:09 -0500

    css prepped and ready

---
+++ script.js
@@ -0,0 +1,16 @@
+// const account_id = "499a2abaee32d4aedc7f28b9b358ef99";
+// const url = `https://api.themoviedb.org/`
+// const config = {method: 'GET',
+// headers: {
+//   accept: 'application/json',
+//   Authorization: 'Bearer 499a2abaee32d4aedc7f28b9b358ef99'
+// }
+// }
+
+// async function getMovies() {
+//     const response = await fetch (url, config)
+//     const data = await response.json();
+//     console.log(data)
+// }
+
+// getMovies()
