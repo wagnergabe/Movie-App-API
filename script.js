4f1eb093c0ff739b42f2a6f567de4a97c8705fdc
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-05-18 23:38:15 -0500

    update javascript/ api working

--- script.js
+++ script.js
@@ -9,20 +9,54 @@ const config = {
   },
 };
 
-
-
 const form = document.getElementById("form");
 const search = document.getElementById("search");
+const main = document.getElementById("main");
 
-// getMovies(api_url);
+getMovies(api_url);
 
 async function getMovies(url) {
   const response = await fetch(url, config);
   const data = await response.json();
-  console.log(data);
+  showMovies(data.results);
 }
 
+function showMovies(movies) {
+  main.innerHTML = "";
 
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
+
+    movieEl.innerHTML = `  
+     
+        <img src ="${img_path + poster_path}"
+        alt ="${title}">
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+          <h3>Overview</h3>
+        ${overview}
+        </div>
+      `
+
+      main.appendChild(movieEl)
+  });
+}
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green'
+    } else if ( vote >= 5 ) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -30,9 +64,9 @@ form.addEventListener("submit", (e) => {
   const searchTerm = search.value;
 
   if (searchTerm && searchTerm !== "") {
-    getMovies(search_url + searchTerm)
+    getMovies(search_url + searchTerm);
 
-    search.value = ''
+    search.value = "";
   } else {
     window.location.reload();
   }
